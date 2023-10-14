0a65da5f003979408661a8ae596b4b9c5d599ef1
Mikolaj Pastuszek <m.pastuszek@appfinity.pl>
2023-10-14 23:30:25 +0200

    add handler for unique exception

--- shopping-list-backend/src/main/java/pl/edu/pwr/pastuszek/shoppinglistbackend/exception/ExceptionHandlers.java
+++ shopping-list-backend/src/main/java/pl/edu/pwr/pastuszek/shoppinglistbackend/exception/ExceptionHandlers.java
@@ -2,19 +2,37 @@ package pl.edu.pwr.pastuszek.shoppinglistbackend.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandlers {
     private static final Logger logger = LoggerFactory.getLogger(ExceptionHandlers.class);
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        Throwable cause = ex.getRootCause();
+        if (cause instanceof SQLException sqlException) {
+            String sqlState = sqlException.getSQLState();
+
+            if ("23505".equals(sqlState)) { // PostgreSQL error code for unique constraint violation
+                String errorMessage = sqlException.getMessage();
+                return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        logger.error("DataIntegrityViolationException: " + ex.getMessage(), ex);
+        return new ResponseEntity<>("Data integrity violation", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(RuntimeException.class)
