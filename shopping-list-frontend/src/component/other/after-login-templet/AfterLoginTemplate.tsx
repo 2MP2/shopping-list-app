88224d08c9846c6e3414d550b0b0364af2987ac9
Mikolaj Pastuszek <m.pastuszek@appfinity.pl>
2023-11-06 09:57:33 +0100

    Add Routes Creat main components, only SingUp, SingIN, AfterLoginTemplate with logic

---
+++ shopping-list-frontend/src/component/other/after-login-templet/AfterLoginTemplate.tsx
@@ -0,0 +1,160 @@
+import * as React from 'react';
+import {styled, useTheme} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Button from '@mui/material/Button';
+import {OrganizationResponseDTO} from "../../../model/dto/response";
+import {useEffect, useState} from "react";
+import {getOrganizationList} from "../../../service/organization";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+const drawerWidth = 240;
+
+const Main = styled('main', {shouldForwardProp: (prop) => prop !== 'open'})<{
+    open?: boolean;
+}>(({theme, open}) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({theme, open}) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const DrawerHeader = styled('div')(({theme}) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
+
+function AfterLoginTemplate() {
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+    const [organizations, setOrganizations] = useState<OrganizationResponseDTO[]>([]);
+
+    useEffect(() => {
+        getOrganizationList({})
+            .then((response) => {
+                setOrganizations(response.content);
+            })
+            .catch((error) => {
+                toast.error("Couldn't load your organization");
+            });
+    }, []);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Persistent drawer
+                    </Typography>
+                    <Button color="inherit" sx={{ marginLeft: 'auto' }}>User</Button>
+                    <Button color="inherit" >Logout</Button>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
+            >
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <Button color="inherit" >CRATE ORGANIZATION</Button>
+                <Divider />
+                <List>
+                    {organizations.map((org) => (
+                        <ListItem key={org.id} disablePadding>
+                            <ListItemButton href={`/organization/${org.id}`}>
+                                <ListItemText primary={org.name} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <Main open={open}>
+                //tu jest środek
+            </Main>
+            <ToastContainer />
+        </Box>
+    );
+}
+
+export default AfterLoginTemplate;
---
+++ shopping-list-frontend/src/page/creat-orgnization/CreatOrganization.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateOrganization() {
+    return (
+        <div>
+            <h1>Create Organization</h1>
+        </div>
+    );
+}
+
+export default CreateOrganization;
