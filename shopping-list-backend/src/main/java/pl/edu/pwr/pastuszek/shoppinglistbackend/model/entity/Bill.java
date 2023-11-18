930996215441204aa24be45ab1099708be8eec6c
Mikolaj Pastuszek <m.pastuszek@appfinity.pl>
2023-11-19 00:03:33 +0100

    change amount from int to BigDecimal add/fix filter by null billId in product add algorithm to count transactions fix UserResponseDTO

--- shopping-list-backend/src/main/java/pl/edu/pwr/pastuszek/shoppinglistbackend/model/entity/Bill.java
+++ shopping-list-backend/src/main/java/pl/edu/pwr/pastuszek/shoppinglistbackend/model/entity/Bill.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
@@ -20,9 +22,11 @@ public class Bill implements Entity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private int amount;
+    private BigDecimal amount;
     private String shop;
     private String comment;
+    @Column(name = "update_time")
+    private Timestamp updateTime = new Timestamp(System.currentTimeMillis());
     @OneToMany(mappedBy = "bill", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
     @ToString.Exclude
     @JsonIgnore
@@ -33,12 +37,6 @@ public class Bill implements Entity {
     @ManyToOne
     @JoinColumn(name = "owner_id", referencedColumnName = "id")
     private User user;
-
-    @PreRemove
-    private void preRemove(){
-        for(Product product: products)
-            product.setBill(null);
-    }
 
     @Override
     public final boolean equals(Object o) {
